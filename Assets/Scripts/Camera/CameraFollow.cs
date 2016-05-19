aec99821459ea72e964b3e366848c6dbaa2655ff
kai <ren.tam@mycit.ie>
2016-05-19 01:48:31 +0100

    Mouse able to move scene movement to allow scene rotation. Buggy due to multi use to mouse for player and scene movement, this causes the player constantly rotating back and for when mouse at certain level.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
+    //Third Person Camera
+    public Transform lookAt;
+    public Transform camTransform;
+    private Camera cam;
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 30.0f;
+
     //Target for camera to follow
     public Transform target;
 
@@ -14,6 +24,8 @@ public class CameraFollow : MonoBehaviour {
 
     void Start()
     {
+        camTransform = transform;
+        cam = Camera.main;
         offset = transform.position - target.position;
     }
 
@@ -26,5 +38,22 @@ public class CameraFollow : MonoBehaviour {
         //Lerp Smoothly move from one position to another
         //move from current position to target position with the smoothing level that set above( * delta time so that it wont do it 50 times per seconds)
         transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+
+        //Dont want to make a y Axis to have a full Loop
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 direction = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * direction;
+        camTransform.LookAt(lookAt.position);
     }
 }
